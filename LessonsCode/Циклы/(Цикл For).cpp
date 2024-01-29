82eb1c01850e7daa3a9bd65d57c94d52935ade42
LFYDRemamberMe <157413813+LFYDRemamberMe@users.noreply.github.com>
2024-01-29 14:31:46 +0200

    Update and rename (Цикл For).txt to (Цикл For).cpp

--- LessonsCode/Циклы/(Цикл For).cpp
+++ LessonsCode/Циклы/(Цикл For).cpp
@@ -6,9 +6,9 @@ void main() {
 	setlocale(LC_ALL, "RU");
 
 	//int i;
-	/*int i = 0;*/
+	int i = 0;
 
-	/*cout << "Первый цикл" << endl;
+	cout << "Первый цикл" << endl;
 	for ( ; i < 10; i++)
 	{
 		cout << "Переменная i: " << i << endl;
@@ -19,19 +19,19 @@ void main() {
 	for ( ; i < 20; i++)
 	{
 		cout << "Переменная i: " << i << endl;
-	}*/
+	}
 
-	/*cout << "Бесконечный цикл" << endl;
+	cout << "Бесконечный цикл" << endl;
 	cout << "#####   #  ####### #####    ########  #### # #" << endl;
 	for ( ; ; )
 	{
 		cout << "Соси хуй" << endl;
-	}*/
+	}
 
-	/*for (int i = 0, j = 10 ; i < 11 && j != 5; i++, j--)
+	/* for (int i = 0, j = 10 ; i < 11 && j != 5; i++, j--)
 	{
 		cout << "Переменная i = " << i << endl;
 		cout << "Переменная j = " << j << endl;
-	}*/
+	} */
 
 }
